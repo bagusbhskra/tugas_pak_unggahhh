4d440fe23f24f49cff77d61e098a2cce41053f00
bagusbhskra <putubagusandikabhaskaraxirpl@gmail.com>
2024-08-26 11:11:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const tombol = document.getElementById('ubahTeks');
+tombol.addEventListener('click', function() {
+    const paragraf = document.getElementById('andika');
+    paragraf.textContent = 'melajah ddpk mule seru';
+});
